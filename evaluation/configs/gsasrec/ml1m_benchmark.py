f7331bae9df6a85b7b1843ce4e8e5cd3c81d6399
Aleksandr (Sasha) Petrov <firexel@gmail.com>
2023-02-27 15:29:41 +0000

    Update ml1m_benchmark.py

--- evaluation/configs/gsasrec/ml1m_benchmark.py
+++ evaluation/configs/gsasrec/ml1m_benchmark.py
@@ -1,5 +1,5 @@
 from aprec.evaluation.split_actions import LeaveOneOut
-from aprec.evaluation.configs.bert4rec_ft.common_benchmark_config import *
+from aprec.evaluation.configs.gsasrec.common_benchmark_config import *
 
 DATASET = "BERT4rec.ml-1m"
 N_VAL_USERS=512
