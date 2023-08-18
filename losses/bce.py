b4cbe672b956db832b1452a0c3da0fe0d95b077e
Aleksandr Petrov <firexel@gmail.com>
2023-08-18 09:10:44 +0100

    use softplus version of bce loss for increased stability

--- losses/bce.py
+++ losses/bce.py
@@ -9,12 +9,12 @@ class BCELoss(ListWiseLoss):
         self.less_is_better = True
 
     def calc_per_list(self, y_true_raw, y_pred):
-        eps = tf.constant(1e-16, y_pred.dtype)
+        eps = tf.constant(1e-8, y_pred.dtype)
         y_true = tf.cast(y_true_raw, y_pred.dtype)
         is_target = tf.cast((y_true >= -eps), y_pred.dtype)
         trues = y_true*is_target
-        pos = -trues*tf.math.log((tf.sigmoid(y_pred) + eps)) * is_target
-        neg = -(1.0 - trues)*tf.math.log((1.0 - tf.sigmoid(y_pred)) + eps) * is_target
+        pos = trues*tf.math.softplus(-y_pred) * is_target
+        neg = (1.0 - trues)*tf.math.softplus(y_pred) * is_target
         num_targets = tf.reduce_sum(is_target, axis=1)
         ce_sum = tf.reduce_sum(pos + neg, axis=1)
         res_sum = tf.math.divide_no_nan(ce_sum, num_targets)
@@ -22,11 +22,11 @@ class BCELoss(ListWiseLoss):
 
     def __call__(self, y_true_raw, y_pred):
         y_true = tf.cast(y_true_raw, y_pred.dtype)
-        eps = tf.constant(1e-16, y_pred.dtype)
+        eps = tf.constant(1e-8, y_pred.dtype)
         is_target = tf.cast((y_true >= -eps), y_pred.dtype)
         trues = y_true*is_target
-        pos = -trues*tf.math.log((tf.sigmoid(y_pred) + eps)) * is_target
-        neg = -(1.0 - trues)*tf.math.log((1.0 - tf.sigmoid(y_pred)) + eps) * is_target
+        pos = trues*tf.math.softplus(-y_pred) * is_target
+        neg = (1.0 - trues)*tf.math.softplus(y_pred) * is_target
         num_targets = tf.reduce_sum(is_target)
         ce_sum = tf.reduce_sum(pos + neg)
         res_sum = tf.math.divide_no_nan(ce_sum, num_targets)
